25b4634b030fc4d9941c4fd6b44c1b04dc101e13
jiayun <jean93327@gmail.com>
2024-05-02 17:47:12 +0800

    feat(product): 建立產品清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  productName = 'A 產品';
-  authors = '作者 A、作者 B、作者 C';
-  company = '博碩文化';
-  isShow = true;
-  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-  price = 1580;
-}
+export class AppComponent {}
---
+++ src/app/product-card-list/product-card-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'app-product-card-list',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './product-card-list.component.html',
+  styleUrl: './product-card-list.component.css',
+})
+export class ProductCardListComponent {
+  productName = 'A 產品';
+  authors = '作者 A、作者 B、作者 C';
+  company = '博碩文化';
+  isShow = true;
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+  price = 1580;
+}
