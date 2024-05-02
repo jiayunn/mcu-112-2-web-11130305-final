b0daac7f45d69920269b0179b12238ae97ade459
jiayun <jean93327@gmail.com>
2024-05-02 16:50:26 +0800

    feat(product): 利用文字插值和屬性繫結實作

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
-
+  productName = 'A 產品';
+  authors = '作者 A、作者 B、作者 C';
+  company = '博碩文化';
+  price = '';
+  isShow = true;
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 }
