ba15edad2ac1d57ad3c0b49ebc95c14542794a2c
jiayun <jean93327@gmail.com>
2024-05-08 11:58:52 +0800

    feat(router): 切換產品清單與購物車頁面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'shoppingcart', component: ShoppingCartComponent },
+];
