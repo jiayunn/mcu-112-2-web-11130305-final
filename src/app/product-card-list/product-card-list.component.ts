cbb184d4349d9041a44806530d68aab60a9ccaf0
jiayun <jean93327@gmail.com>
2024-06-03 19:00:25 +0800

    feat(pagination): 新增分頁元件

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,11 +1,12 @@
 import { Product } from './../model/product';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-card-list',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.css',
 })
@@ -15,4 +16,6 @@ export class ProductCardListComponent {
 
   @Output()
   view = new EventEmitter<Product>();
+
+  pageIndex = 1;
 }
