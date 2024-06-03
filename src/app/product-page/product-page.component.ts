e7dfdd669ff63a2d9de497db17b7eebeb6b710cc
jiayun <jean93327@gmail.com>
2024-06-03 19:27:12 +0800

    feat(product): 查詢列設定

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,16 @@
 import { ProductService } from './../services/product.service';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Router } from '@angular/router';
 import { Subject, startWith, switchMap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [AsyncPipe, ProductCardListComponent],
+  imports: [AsyncPipe, JsonPipe, ReactiveFormsModule, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
@@ -20,9 +21,11 @@ export class ProductPageComponent {
 
   private readonly refresh$ = new Subject<void>();
 
+  protected readonly formControl = new FormControl<string | undefined>(undefined);
+
   readonly product$ = this.refresh$.pipe(
     startWith(undefined),
-    switchMap(() => this.productService.getList('B產品', 1, 5))
+    switchMap(() => this.productService.getList(undefined, 1, 5))
   );
 
   onView(product: Product): void {
