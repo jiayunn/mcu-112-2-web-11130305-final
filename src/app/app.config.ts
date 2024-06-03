7e923c36573fb3dc6d94589ee08d5c2dce5e68df
jiayun <jean93327@gmail.com>
2024-06-03 16:25:48 +0800

    feat(product): 從後端取得產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { ProductRemoteService } from './services/product-remote.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    { provide: ProductService, useClass: ProductRemoteService },
+  ],
 };
